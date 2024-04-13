26d9cfe62f711a909eca019e4a617ce8f2c1a0e5
Valentín Ottaviano <valentinottaviano@gmail.com>
2024-04-13 19:24:27 -0300

    initial commit

---
+++ src/components/MobileNav.tsx
@@ -0,0 +1,27 @@
+import { Icon } from "@iconify/react"
+import { Button } from "./ui/button"
+import {
+  Sheet,
+  SheetContent,
+  SheetTrigger,
+} from "./ui/sheet"
+
+
+export const MobileNav = (props: any) => {
+
+  return <Sheet>
+    <SheetTrigger>
+      <Button variant="outline" className="w-10 h-10 p-0">
+        <Icon icon="mdi:menu" className="text-xl" />
+      </Button>
+    </SheetTrigger>
+    <SheetContent>
+      <div className="h-full flex flex-col items-center justify-center gap-5">
+        {props.routes.map((route: any) => {
+          return <a key={route.path} href={route.path}>{route.name}</a>
+        })}
+      </div>
+    </SheetContent>
+  </Sheet>
+}
+
